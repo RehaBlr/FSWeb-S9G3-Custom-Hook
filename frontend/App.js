f199580817e7da29762b073056dd7a511ba3c7da
Reha <134259610+RehaBlr@users.noreply.github.com>
2023-07-26 23:53:31 +0300

    done

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import axios from "axios";
 
-// import Charts from "./components/Charts";
+import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import geceModuAc from "./hooks/geceModuAc";
 
 const App = () => {
-  // const [coinData, setCoinData] = useState([]);
+  const [coinData, setCoinData] = useState([]);
   // const [geceModu, setGeceModu] = useState(false);
   const [geceModu, setGeceModu] = geceModuAc(false);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-  //     )
-  //     .then((res) => setCoinData(res.data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+      )
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className={geceModu ? "dark-mode App" : "App"}>
       <Navbar geceModu={geceModu} setGeceModu={setGeceModu} />
-      {/* <Charts coinData={coinData} /> */}
+      <Charts coinData={coinData} />
     </div>
   );
 };
